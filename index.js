243820dd93971a44594c320e6cf36b4028f02243
wazyiz <codebywaz@gmail.com>
2022-04-05 12:51:42 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+});
+
+mongoose
+	.connect(MONGODB, { useNewUrlParser: true })
+	.then(() => {
+		console.log('MongoDB Connected!');
+		return 
+server.listen({ port: 5000 });
+	})
+	.then((res) => {
+		console.log(`Umphaki ubaleka ku ${res}`);
+	});
